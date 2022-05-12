017aaad727cfbbf3269f4a6083d728b0b7b4e4f5
RavinduAX <ravinduadassuriya59@gmail.com>
2022-05-12 11:46:25 +0530

    Added a Custom Layer of interfaces to implement unique method for DAO Layer

--- Layered_Architecture/src/dao/ItemDAOImpl.java
+++ Layered_Architecture/src/dao/ItemDAOImpl.java
@@ -8,58 +8,92 @@ import java.math.BigDecimal;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class ItemDAOImpl implements CrudDAO<ItemDTO, String> {
-
-    public ArrayList<ItemDTO> getAll() throws SQLException, ClassNotFoundException {
-        ResultSet rst = SQLUtil.executeQuery("SELECT * FROM Item");
-
-        ArrayList<ItemDTO> allItem = new ArrayList<>();
-
-        while(rst.next()){
-            String code = rst.getString(1);
-            String description = rst.getString(2);
-            BigDecimal price = rst.getBigDecimal(4);
-            int qtyOnHand = rst.getInt(3);
-
-            allItem.add(new ItemDTO(code, description, price, qtyOnHand));
-        }
-        return allItem;
-    }
-
-    public boolean delete(String code) throws SQLException, ClassNotFoundException {
-           return SQLUtil.executeUpdate("DELETE FROM Item WHERE id=?",code);
-    }
-
-    public boolean save(ItemDTO dto) throws SQLException, ClassNotFoundException {
-    return SQLUtil.executeUpdate("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)",dto.getCode(),dto.getDescription(),dto.getUnitPrice(),dto.getQtyOnHand());
-    }
-
-    public boolean update(ItemDTO dto) throws SQLException, ClassNotFoundException {
-    return SQLUtil.executeUpdate("UPDATE Item SET description=?, unitPrice=?, qtyOnHand=? WHERE code=?",dto.getDescription(),dto.getUnitPrice(),dto.getQtyOnHand(),dto.getCode());
-    }
-
+public class ItemDAOImpl implements ItemDAO {
     @Override
-    public ItemDTO search(String code) throws SQLException, ClassNotFoundException {
-        ResultSet rst = SQLUtil.executeQuery("SELECT * FROM Item WHERE code=?", code);
-        if(rst.next()){
-            return new ItemDTO(rst.getString(1), rst.getString(2), rst.getBigDecimal(4), rst.getInt(3));
-        }
+    public ArrayList<ItemDTO> getAll() throws SQLException, ClassNotFoundException {
         return null;
     }
 
-    public boolean exist(String code) throws SQLException, ClassNotFoundException {
-    return SQLUtil.executeQuery("SELECT code FROM Item WHERE code=?",code).next();
+    @Override
+    public boolean save(ItemDTO dto) throws SQLException, ClassNotFoundException {
+        return false;
     }
 
-    public String genarateNewId() throws SQLException, ClassNotFoundException {
-        ResultSet rst = SQLUtil.executeQuery("SELECT code FROM Item ORDER BY code DESC LIMIT 1;");
-        if (rst.next()) {
-            String id = rst.getString("code");
-            int newItemId = Integer.parseInt(id.replace("I00-", "")) + 1;
-            return String.format("I00-%03d", newItemId);
-        } else {
-            return "I00-001";
-        }
+    @Override
+    public boolean update(ItemDTO dto) throws SQLException, ClassNotFoundException {
+        return false;
     }
+
+    @Override
+    public ItemDTO search(String s) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean exist(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String genarateNewId() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+//    public ArrayList<ItemDTO> getAll() throws SQLException, ClassNotFoundException {
+//        ResultSet rst = SQLUtil.executeQuery("SELECT * FROM Item");
+//
+//        ArrayList<ItemDTO> allItem = new ArrayList<>();
+//
+//        while(rst.next()){
+//            String code = rst.getString(1);
+//            String description = rst.getString(2);
+//            BigDecimal price = rst.getBigDecimal(4);
+//            int qtyOnHand = rst.getInt(3);
+//
+//            allItem.add(new ItemDTO(code, description, price, qtyOnHand));
+//        }
+//        return allItem;
+//    }
+//
+//    public boolean delete(String code) throws SQLException, ClassNotFoundException {
+//           return SQLUtil.executeUpdate("DELETE FROM Item WHERE id=?",code);
+//    }
+//
+//    public boolean save(ItemDTO dto) throws SQLException, ClassNotFoundException {
+//    return SQLUtil.executeUpdate("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)",dto.getCode(),dto.getDescription(),dto.getUnitPrice(),dto.getQtyOnHand());
+//    }
+//
+//    public boolean update(ItemDTO dto) throws SQLException, ClassNotFoundException {
+//    return SQLUtil.executeUpdate("UPDATE Item SET description=?, unitPrice=?, qtyOnHand=? WHERE code=?",dto.getDescription(),dto.getUnitPrice(),dto.getQtyOnHand(),dto.getCode());
+//    }
+//
+//    @Override
+//    public ItemDTO search(String code) throws SQLException, ClassNotFoundException {
+//        ResultSet rst = SQLUtil.executeQuery("SELECT * FROM Item WHERE code=?", code);
+//        if(rst.next()){
+//            return new ItemDTO(rst.getString(1), rst.getString(2), rst.getBigDecimal(4), rst.getInt(3));
+//        }
+//        return null;
+//    }
+//
+//    public boolean exist(String code) throws SQLException, ClassNotFoundException {
+//    return SQLUtil.executeQuery("SELECT code FROM Item WHERE code=?",code).next();
+//    }
+//
+//    public String genarateNewId() throws SQLException, ClassNotFoundException {
+//        ResultSet rst = SQLUtil.executeQuery("SELECT code FROM Item ORDER BY code DESC LIMIT 1;");
+//        if (rst.next()) {
+//            String id = rst.getString("code");
+//            int newItemId = Integer.parseInt(id.replace("I00-", "")) + 1;
+//            return String.format("I00-%03d", newItemId);
+//        } else {
+//            return "I00-001";
+//        }
+//    }
 
 }
--- Layered_Architecture/src/dao/OrderDetailsDAOImpl.java
+++ Layered_Architecture/src/dao/OrderDetailsDAOImpl.java
@@ -8,40 +8,41 @@ import model.OrderDetailDTO;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class OrderDetailsDAOImpl implements CrudDAO<OrderDetailDTO, String> {
-    @Override
-    public ArrayList<OrderDetailDTO> getAll() throws SQLException, ClassNotFoundException {
-        return null;
-    }
+public class OrderDetailsDAOImpl implements OrderDetailsDAO {
 
-    @Override
-    public boolean save(OrderDetailDTO dto) throws SQLException, ClassNotFoundException {
-        return SQLUtil.executeUpdate("INSERT INTO OrderDetails (oid, itemCode, unitPrice, qty) VALUES (?,?,?,?)",dto.getOid(), dto.getItemCode(), dto.getUnitPrice(), dto.getQty());
-    }
-
-    @Override
-    public boolean update(OrderDetailDTO dto) throws SQLException, ClassNotFoundException {
-        return false;
-    }
-
-    @Override
-    public OrderDetailDTO search(String s) throws SQLException, ClassNotFoundException {
-        return null;
-    }
-
-    @Override
-    public boolean exist(String s) throws SQLException, ClassNotFoundException {
-        return false;
-    }
-
-    @Override
-    public boolean delete(String s) throws SQLException, ClassNotFoundException {
-        return false;
-    }
-
-    @Override
-    public String genarateNewId() throws SQLException, ClassNotFoundException {
-        return null;
-    }
+//    @Override
+//    public ArrayList<OrderDetailDTO> getAll() throws SQLException, ClassNotFoundException {
+//        return null;
+//    }
+//
+//    @Override
+//    public boolean save(OrderDetailDTO dto) throws SQLException, ClassNotFoundException {
+//        return SQLUtil.executeUpdate("INSERT INTO OrderDetails (oid, itemCode, unitPrice, qty) VALUES (?,?,?,?)",dto.getOid(), dto.getItemCode(), dto.getUnitPrice(), dto.getQty());
+//    }
+//
+//    @Override
+//    public boolean update(OrderDetailDTO dto) throws SQLException, ClassNotFoundException {
+//        return false;
+//    }
+//
+//    @Override
+//    public OrderDetailDTO search(String s) throws SQLException, ClassNotFoundException {
+//        return null;
+//    }
+//
+//    @Override
+//    public boolean exist(String s) throws SQLException, ClassNotFoundException {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean delete(String s) throws SQLException, ClassNotFoundException {
+//        return false;
+//    }
+//
+//    @Override
+//    public String genarateNewId() throws SQLException, ClassNotFoundException {
+//        return null;
+//    }
 
 }
