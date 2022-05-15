98c53216211f431f0b1a0eceab99b3e48e6302c7
RavinduAX <ravinduadassuriya59@gmail.com>
2022-05-15 14:10:23 +0530

    Itroducing Factory to the DAO

--- Layered_Architecture/src/dao/DAOFactory.java
+++ Layered_Architecture/src/dao/DAOFactory.java
@@ -12,4 +12,23 @@ public class DAOFactory {
         return daoFactory == null ? daoFactory=new DAOFactory() : daoFactory ;
     }
 
+    public enum DAOTypes {
+        CUSTOMER, ITEM, ORDER, ORDERDETAILS,QUERYDAO
+    }
+
+    public void getDAO(DAOTypes types){
+        switch (types) {
+            case CUSTOMER:
+                return;
+            case ITEM:
+                return;
+            case ORDER:
+                return;
+            case ORDERDETAILS:
+                return;
+            case QUERYDAO:
+                return;
+        }
+    }
+
 }
